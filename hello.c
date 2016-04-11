53f4b9f75799e8fa77b9b67348f5fcbb5144cc8b
Prabhakaran Senthilnathan <prabhakaran9397@gmail.com>
2016-04-11 20:42:57 +0530

    test programs added

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("Hello world in C\n");
+	return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello world in CPP" << endl;
+	return 0;
+}
